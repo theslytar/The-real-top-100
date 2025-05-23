c2af8d221d66f9c2254e7837e1afd89e82c44245
theslytar <alsultan.sumo@gmail.com>
2025-05-23 18:08:40 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,14 @@
-module.exports = { reactStrictMode: true }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+
+  // ⬇️ allow external images from Futbin & FlagCDN
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'cdn.futbin.com' },
+      { protocol: 'https', hostname: 'flagcdn.com' }
+    ]
+  }
+};
+
+module.exports = nextConfig;
